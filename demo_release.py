2d13a1b8a21fd03504cc68bf5496b8fe6c188e6e
Alexander Chernykh <alexanderchernykh.ru@gmail.com>
2021-10-23 22:55:44 +0200

    Updated logging

--- demo_release.py
+++ demo_release.py
@@ -6,25 +6,21 @@ import http
 # from flask_cors import CORS, cross_origin
 from flask import request, Flask, Response, jsonify, abort
 import json
-
+import logging
 
 app = Flask(__name__)
-
-
-
 
 # def get_weights():
 global colorizer_eccv16
 colorizer_eccv16 = eccv16(pretrained=True).eval()
-
+logger = logging.getLogger('example_logger')
 
 def colorise(data):
-
     try:
-        print(f"GOT {data['path']}")
+        logger.warning(f"GOT {data['path']}")
         img = load_img(data["path"])
     except:
-        print(f"COULD NOT OPEN FILE {data['path']}")
+        logger.warning(f"COULD NOT OPEN FILE {data['path']}")
         return json.dumps(http.HTTPStatus.BAD_REQUEST, ensure_ascii=False).encode('utf8')
 
     (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(256, 256))
@@ -32,14 +28,12 @@ def colorise(data):
     out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
 
     try:
-        print(f"TRYING TO SAVE /imgs/colorised/{os.path.basename(data['path'])}")
+        logger.warning(f"TRYING TO SAVE /imgs/colorised/{os.path.basename(data['path'])}")
         plt.imsave(f"/imgs/colorised/{os.path.basename(data['path'])}", out_img_eccv16)
         return json.dumps(http.HTTPStatus.OK, ensure_ascii=False).encode('utf8')
     except:
-        print(f"Could not save  ./imgs/colorised/{os.path.basename(data['path'])}")
+        logger.warning(f"Could not save  ./imgs/colorised/{os.path.basename(data['path'])}")
         return json.dumps(http.HTTPStatus.BAD_REQUEST, ensure_ascii=False).encode('utf8')
-
-
 
 
 @app.route('/colorised', methods=['POST'])
@@ -50,9 +44,7 @@ def coloriseee():
     return ret
 
 
-
 if __name__ == '__main__':
     # col = Coloriser()
     # get_weights()
     app.run('0.0.0.0', port=2020)
-
